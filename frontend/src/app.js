6e26e473e03bae849941c6e7211893ce85995b62
Nandan Reddy <nandan.reddy1133@gmail.com>
2021-08-28 01:27:27 +0530

    Testing for deployment

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,7 +3,11 @@ import Auth from './components/auth/auth';
 
 function App() {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	return <React.Fragment>{!isLoggedIn && <Auth />}</React.Fragment>;
+	return (
+		<React.Fragment>
+			{!isLoggedIn && <Auth fun={setIsLoggedIn} />}
+		</React.Fragment>
+	);
 }
 
 export default App;
