31b9423850425bcb0102b3d31f8486daeb23f5a4
Nandan Reddy <nandan.reddy1133@gmail.com>
2021-09-01 02:02:19 +0530

    Resolved a warning

--- frontend/src/components/chat/chatSpace.js
+++ frontend/src/components/chat/chatSpace.js
@@ -7,7 +7,7 @@ import Message from './message/message';
 function ChatSpace({ socket }) {
 	// console.log(activeChat);
 	const activeChat = useSelector((state) => state.chat.activeChat);
-	const { user: currentUser } = useSelector((state) => state.app);
+	// const { user: currentUser } = useSelector((state) => state.app);
 	// console.log(currentUser);
 	const messages = activeChat.messages.map((message, ind) => (
 		<Message key={ind} {...message} />
