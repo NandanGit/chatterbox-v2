5791bbc42cecf3adeb74a729b2ff5c4da7781e8e
Nandan Reddy <nandan.reddy1133@gmail.com>
2021-09-01 19:49:07 +0530

    Outgoing direct messages work

--- frontend/src/components/chat/message/messageBox.js
+++ frontend/src/components/chat/message/messageBox.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Icon } from '@iconify/react';
 
 import './messageBox.css';
+import { useSelector } from 'react-redux';
 
-function MessageBox(props) {
+function MessageBox({ socket }) {
+	const { activeChat } = useSelector((state) => state.chat);
+	const inputRef = useRef();
+	const receiverName = activeChat.username || activeChat.groupName;
+	const receiverType = activeChat.type;
+
+	const sendMessageHandler = (event) => {
+		event.preventDefault();
+		const message = inputRef.current.value.trim();
+		if (!message) {
+			return;
+		}
+		if (receiverType === 'user') {
+			socket.emit(
+				'direct:message',
+				{ to: receiverName, body: message },
+				({ isSent, message }) => {}
+			);
+		} else {
+			socket.emit(
+				'group:message',
+				{ to: receiverName, body: message },
+				({ isSent, message }) => {}
+			);
+		}
+		inputRef.current.value = '';
+	};
+
 	return (
 		<div className="message-box">
-			<form>
-				<input id="message-box-space" />
+			<form onSubmit={sendMessageHandler}>
+				<input
+					id="message-box-space"
+					ref={inputRef}
+					placeholder="Say something"
+					autoComplete="off"
+				/>
 				<button className="send-message-button">
 					<Icon
 						icon="fluent:send-16-filled"
