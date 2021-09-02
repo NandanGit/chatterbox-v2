57019fc89b9aafe66777f2ef60a7b12cc5ea48eb
Nandan Reddy <nandan.reddy1133@gmail.com>
2021-09-03 00:56:02 +0530

    Added smooth scrolling to messages

--- frontend/src/components/chat/chatSpace.js
+++ frontend/src/components/chat/chatSpace.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 
 import './chatSpace.css';
 import Message from './message/message';
 
 function ChatSpace({ socket }) {
+	const endRef = useRef();
 	const activeChat = useSelector((state) => state.chat.activeChat);
 	const messages = activeChat.messages.map((message, ind) => (
 		<Message key={ind} {...message} />
 	));
-	return <div className="chat-space">{messages}</div>;
+
+	const scrollToBottom = () => {
+		endRef.current?.scrollIntoView({ behavior: 'smooth' });
+	};
+
+	useEffect(() => {
+		scrollToBottom();
+	}, [messages]);
+
+	return (
+		<div className="chat-space">
+			{messages}
+			<div ref={endRef} />
+		</div>
+	);
 }
 
 export default ChatSpace;
